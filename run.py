b731fbcccf7ec5f79d55c24caf6071b309e35767
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-23 02:04:11 +0000

    导航页增加三种主题切换：1.紫蓝渐变毛玻璃 2.低饱和阴影 3.点阵背景

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-23 01:40:28"
-ts = 1784770828
+ver = "2026-07-23 02:04:11"
+ts = 1784772251
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
