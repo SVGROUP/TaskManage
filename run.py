d89628636b5299dc4a2e3f185a4d9e02e784feec
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-23 06:31:24 +0000

    导航页网格紧凑化：minmax(72px, 1fr) + 减小间距，自适应一行尽量多放

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-23 06:23:03"
-ts = 1784787783
+ver = "2026-07-23 06:31:24"
+ts = 1784788284
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
