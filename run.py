481fc0e6b6929bbb2c6debe153c1e497671c0a13
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-23 11:38:32 +0000

    修复：周/日视图时间轴任务也加多选框，JS版本v36->v37

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-23 11:11:45"
-ts = 1784805105
+ver = "2026-07-23 11:38:32"
+ts = 1784806712
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
