91fe7bc21d47d54199a79c2dcda2dfd143005491
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-23 15:35:43 +0000

    fix: 修正 upgrade.py UPGRADE_TAG 默认值 TaskManage → TASK

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-23 14:55:24"
-ts = 1784818524
+ver = "2026-07-23 15:35:43"
+ts = 1784820943
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
